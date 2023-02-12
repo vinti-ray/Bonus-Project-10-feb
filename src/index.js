109ec923552fbaa369d306ab9151faffd507e5d1
vinti-roy <vintiray71@gmail.com>
2023-02-12 12:49:10 +0530

    dsfd

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 const express=require("express")
 const app=express()
 const cron = require("node-cron");
+const path=require("path")
 
 
 const events = [
-    {  text: 'first text', dateTime: '*/5 * * * *' },
-    { text: 'second text', dateTime: '*/6 * * * *' },
-    {  text: 'third text', dateTime: '*/7 * * * *' },
-    {  text: 'fourth text', dateTime: '*/8 * * * *' },
-    {  text: 'fifth text', dateTime: '*/9 * * * *' },
-    {  text: 'sixth text', dateTime: '*/10 * * * *' },
-    { text: 'seventh text', dateTime: '*/11 * * * *' },
-    {  text: 'eighth text', dateTime: '*/12 * * * *' },
-    {  text: 'ninth text', dateTime: '*/13 * * * *' },
-    {  text: 'tenth text', dateTime: '*/14 * * * *' }
+    {      text: "textOne", dateTime: "34 47 12 12 02 *"},  //specifies that the job will run at 2:23:20 PM on the 12th day of the 11th month.
+    {     text: "textTwo", dateTime: "34 34 11 12 02 *" },
+    {     text: "textThree", dateTime: "34 34 11 12 02 *" },
+    {      text: "textFour", dateTime: "34 34 11 12 02  *"},
+    {      text: "textFive",dateTime: "34 34 11 12 02 *"},
+    {  text: 'textSix', dateTime: "34 34 11 12 02  *" },
+    { text: 'textSeven', dateTime: "34 34 11 12 02  *" },
+    {  text: 'textEight', dateTime: "34 34 11 12 02 *"},
+    {  text: 'textNine', dateTime: "34 34 11 12 02 *" }, 
+    {  text: 'textTen', dateTime: "34 34 11 12 02 *" } 
   ];
-
-
+ 
 //   function scheduleEvent(text) {
 //     console.log(`Scheduling event: ${text.text}`);
 //     cron.schedule(text.dateTime, () => {
@@ -24,23 +24,38 @@ const events = [
 //       return res.send({message:text.text})
 //     });
 //   }
+
+app.use("/cronAssignment/:text",(req,res)=>{
+   let text=req.params.text
+
   
 
-app.use("/cronAssignment",(req,res)=>{
-    // for (const text of events) {
-    //     scheduleEvent(text);
-    //   }
-    for(let event of events){
-        cron.schedule(event.dateTime, function() {
-            console.log(`printing the  ${event.text}`);
+  events.forEach((event) => {
+      if(event.text==text){cron.schedule(event.dateTime, function() {
+            console.log(`printing   ${event.text}`);
             return res.send({message:event.text})
-        });
+        })}
+  });
 
-        
-    }
+  
+
 
 })
 
+// app.use("/cronAssignment",(req,res)=>{
+//     // for (const text of events) {
+//     //     scheduleEvent(text);
+//     //   }
+//     for(let event of events){
+//         cron.schedule(event.dateTime, function() {
+//             console.log(`printing the  ${event.text}`);
+//             return res.send({message:event.text})
+//         });
+ 
+        
+//     }
+
+// })
 
 
 
