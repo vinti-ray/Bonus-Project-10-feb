4abbdc1862763ebc12d2d1d23ea1a588e29caf43
vinti-roy <vintiray71@gmail.com>
2023-02-13 08:01:37 +0530

    dkjf

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const express=require("express")
 const app=express()
 const cron = require("node-cron");
-const path=require("path")
-
 
 const events = [
     {      text: "textOne", dateTime: "34 47 12 12 02 *"},  //specifies that the job will run at 2:23:20 PM on the 12th day of the 11th month.
@@ -17,18 +15,10 @@ const events = [
     {  text: 'textTen', dateTime: "34 34 11 12 02 *" } 
   ];
  
-//   function scheduleEvent(text) {
-//     console.log(`Scheduling event: ${text.text}`);
-//     cron.schedule(text.dateTime, () => {
-//       console.log(`Running event: ${text.text}`);
-//       return res.send({message:text.text})
-//     });
-//   }
+
 
 app.use("/cronAssignment/:text",(req,res)=>{
    let text=req.params.text
-
-  
 
   events.forEach((event) => {
       if(event.text==text){cron.schedule(event.dateTime, function() {
@@ -37,25 +27,8 @@ app.use("/cronAssignment/:text",(req,res)=>{
         })}
   });
 
-  
-
-
 })
 
-// app.use("/cronAssignment",(req,res)=>{
-//     // for (const text of events) {
-//     //     scheduleEvent(text);
-//     //   }
-//     for(let event of events){
-//         cron.schedule(event.dateTime, function() {
-//             console.log(`printing the  ${event.text}`);
-//             return res.send({message:event.text})
-//         });
- 
-        
-//     }
-
-// })
 
 
 
