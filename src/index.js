4e27f2a3fae3f019bdd2dbbb228c704875c1d4b0
vinti-roy <vintiray71@gmail.com>
2023-02-13 08:21:33 +0530

    dkjf

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express=require("express")
 const app=express()
 const cron = require("node-cron");
+const path=require("path")
+
 
 const events = [
-    {      text: "textOne", dateTime: "34 47 12 12 02 *"},  //specifies that the job will run at 2:23:20 PM on the 12th day of the 11th month.
-    {     text: "textTwo", dateTime: "34 34 11 12 02 *" },
-    {     text: "textThree", dateTime: "34 34 11 12 02 *" },
+    {      text: "textOne", dateTime: "34 16 08 13 02 *"},  //specifies that the job will run at 8:16:34 AM on the 13th day of the 2nd month.
+    {     text: "textTwo", dateTime: "34 17 08 13 02 *" },
+    {     text: "textThree", dateTime: "34 18 08 13 02 *" },
     {      text: "textFour", dateTime: "34 34 11 12 02  *"},
     {      text: "textFive",dateTime: "34 34 11 12 02 *"},
     {  text: 'textSix', dateTime: "34 34 11 12 02  *" },
@@ -17,9 +19,26 @@ const events = [
  
 
 
+  //1st aproach
+
+  app.use("/cronAssignment/",(req,res)=>{
+  events.forEach((event) => {
+  function scheduleEvent(text) {
+    console.log(`Scheduling event: ${text}`);
+    cron.schedule(event.dateTime, () => {
+      console.log(`Running event: ${text}`);
+      return res.send({message:text})
+    });
+  }
+  let text=event.text
+  scheduleEvent(text)
+});
+});
+
+
+//2nd approach
 app.use("/cronAssignment/:text",(req,res)=>{
    let text=req.params.text
-
   events.forEach((event) => {
       if(event.text==text){cron.schedule(event.dateTime, function() {
             console.log(`printing   ${event.text}`);
@@ -28,9 +47,6 @@ app.use("/cronAssignment/:text",(req,res)=>{
   });
 
 })
-
-
-
 
 app.listen(3009,()=>{
     console.log("server started");
